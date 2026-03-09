e23892687fe84a5f4f2222a3a496d5e6a870ea27
pofany <148377278+pofany@users.noreply.github.com>
2026-03-09 17:40:36 +0200

    Added azure function app to the infra

--- main.tf
+++ main.tf
@@ -4,25 +4,24 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.prefix}-storage"
+  name                     = "team1torage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
   tags = {
     environment = var.environment
   }
 }
 
 resource "azurerm_storage_share" "pdf_share" {
-  name                 = "${var.prefix}-pdf-files"
+  name                 = "team1pdffiles"
   storage_account_name = azurerm_storage_account.storage.name
   quota                = 50
 }
 
 resource "azurerm_storage_container" "blob_container" {
-  name                  = "${var.prefix}-blob-container"
+  name                  = "team1blobcontainer"
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
---
+++ terraform/function.tf
@@ -0,0 +1,18 @@
+resource "azurerm_service_plan" "plan" {
+  name                = "ocr-function-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_function_app" "python_function" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+  os_type                    = "linux"
+  version                    = "~3"
+}
