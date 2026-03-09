a8be073de89f67df33b219c9aebdd045f726e95a
pofany <148377278+pofany@users.noreply.github.com>
2026-03-09 22:37:47 +0200

    Added backend state to the provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "BeStrongTeam1"
-  #   storage_account_name = "team1storageac"
-  #   container_name       = "tfstate"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "BeStrongTeam1"
+    storage_account_name = "team1storageac"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
