c67448b8152ba987b1c81d839df29d5b25c861dd
pofany <148377278+pofany@users.noreply.github.com>
2026-03-09 18:11:16 +0200

    Full infra

---
+++ terraform/ai.tf
@@ -0,0 +1,15 @@
+resource "azurerm_cognitive_account" "doc_intelligence" {
+  name                = "ocr-doc-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FormRecognizer"
+  sku_name            = "S0"
+}
+
+resource "azurerm_cognitive_account" "openai" {
+  name                = "ocr-openai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "OpenAI"
+  sku_name            = "S0"
+}
